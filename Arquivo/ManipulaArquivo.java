1a1ea453404f73f619a051c393cc365028906757
Ester <esterdasilvabertolani@gmail.com>
2025-10-28 20:44:30 -0300

    Update ManipulaArquivo.java
    adicionei os métodos pra gerar e ler o arquivo com alguns auxiliares, falta o ordenar

--- Arquivo/ManipulaArquivo.java
+++ Arquivo/ManipulaArquivo.java
@@ -1,5 +1,78 @@
 package Arquivo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ManipulaArquivo {
+
+    // ================== Métodos de manipulação do arquivo ==================
+
+    public static String gerarArquivo(int qtdNum, String nomeArquivo) {
+        int[] numeros = gerarNum(qtdNum);
+
+        try (BufferedWriter gerar = new BufferedWriter(new FileWriter(nomeArquivo))) {
+            for (int i : numeros) {
+                gerar.write(String.valueOf(i));
+                gerar.newLine();
+            }
+            return "Arquivo \"" + nomeArquivo + "\" criado com sucesso contendo " + numeros.length + " números!";
+
+        } catch (Exception e) {
+            return "Erro ao criar o arquivo \"" + nomeArquivo + "\": " + e.getMessage();
+        }
+    }
+
+    public static int[] lerArquivo(String nomeArquivo)
+            throws IOException, FileNotFoundException {
+
+        int[] numeros = new int[tamArquivo(nomeArquivo)];
+
+        try (BufferedReader ler = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            int i = 0;
+
+            while ((linha = ler.readLine()) != null) {
+                numeros[i] = Integer.parseInt(linha.trim());
+                i++;
+            }
+        }
+
+        if (numeros.length > 0) {
+            return numeros;
+        } else {
+            return null;
+        }
+
+    }
+
+    public static String ordenarArquivo(String nomeArquivo) {
+        return "";
+    }
+
+    // ================== Métodos private auxiliares ==================
+
+    private static int[] gerarNum(int qtdNum) {
+        int[] numeros = new int[qtdNum];
+        for (int i = 0; i < qtdNum; i++) {
+            numeros[i] = (int) (Math.random() * 1000);
+        }
+        return numeros;
+    }
+
+    private static int tamArquivo(String nomeArquivo) { // pra ver quantas linhas o arquivo tem
+        int cont = 0; // e basear o tamanho do vetor a partir disso
+        try (BufferedReader reader = new BufferedReader(new FileReader(nomeArquivo))) {
+            while ((reader.readLine()) != null) {
+                cont++;
+            }
+        } catch (IOException e) {
+            return 0;
+        }
+        return cont;
+    }
 
 }
