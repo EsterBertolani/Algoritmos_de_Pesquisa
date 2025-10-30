9422b68020f8b52faeb91d2e5ba33db9903747da
Ester <esterdasilvabertolani@gmail.com>
2025-10-30 00:05:02 -0300

    bug

--- Dados/Resultado.java
+++ Dados/Resultado.java
@@ -2,4 +2,26 @@ package Dados;
 
 public class Resultado {
 
+    private int posicao;
+    private long comparacoes;
+    private long tempoExecucao;
+
+    public Resultado(int pos, long comp, long temp) {
+        this.posicao = pos;
+        this.comparacoes = comp;
+        this.tempoExecucao = temp;
+    }
+
+    public int getPosicao() {
+        return posicao;
+    }
+
+    public long getComparacoes() {
+        return comparacoes;
+    }
+
+    public long getTempoExecucao() {
+        return tempoExecucao;
+    }
+
 }
